9fb1df7c6f4c021c26d3ab7b987e8a1dcc1b7d7c
DHITIFOUNDATION <dhitiapp@gmail.com>
2021-11-21 16:13:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,78 @@
+
+from flask import Flask,render_template,request,g,url_for,redirect,make_response
+import os
+import json,time
+from firebase import Firebase
+config = {
+    "apiKey": "AIzaSyDZz8e_QIruwWnyOclDfnPQEjKvCQEZ1GE",
+    "authDomain": "nitk-attendance.firebaseapp.com",
+    "databaseURL": "https://nitk-attendance-default-rtdb.firebaseio.com",
+    "projectId": "nitk-attendance",
+    "storageBucket": "nitk-attendance.appspot.com",
+    "messagingSenderId": "911144952264",
+    "appId": "1:911144952264:web:37ab1feabfe1927bc5e0fd",
+    "measurementId": "G-V3LM0Y97FC"
+  };
+
+firebase = Firebase(config)
+db = firebase.database()
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+def handle_catch(caller, on_exception):
+    try:
+         return caller()
+    except:
+         return on_exception
+
+
+
+@app.route("/",methods = ['GET','POST'])
+def index():
+    if request.method == 'POST':
+        uname = request.form.get("username")     
+        resp = make_response(render_template("dashboard.html",handle_catch = handle_catch ))
+        verified = db.child("Users").child(uname).child("verified").get().val()
+        if(verified!=1):
+            resp.set_cookie('__user__', uname)
+            db.child("Users").child(uname).update({
+                'verified':1
+            })
+            return resp
+        else:
+            return f"User {uname} Already Verfied with some other device!!"
+            
+
+
+    if request.cookies.get('__user__')is not None:
+        verified = db.child("Users").child(request.cookies.get('__user__')).child("verified").get().val()
+        if(verified==1):
+            return render_template("dashboard.html",handle_catch = handle_catch)
+        else:
+            return "You Are not the Verified!!!"
+
+    return render_template("index.html")
+
+@app.route("/verify",methods = ['GET','POST'])
+def verify():
+    if request.method == 'POST':
+        value = request.get_json()
+        db.child("_Attendance_").child(value['key']).set({
+            'RollNo':request.cookies.get('__user__'),
+            "datetime":value['date'],
+            "verified":1
+        })
+        
+    return render_template("success.html")
+
+
+
+
+
+
+
+
+
+
---
+++ static/src/qrCodeScanner.js
@@ -0,0 +1,90 @@
+
+ function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition);
+  } else {
+    div.innerHTML = "The Browser Does not Support Geolocation";
+  }
+}
+function showPosition(position) {
+  div.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+
+
+const qrcode_ = window.qrcode;
+
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
+
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
+const btnScanQR = document.getElementById("btn-scan-qr");
+
+let scanning = false;
+
+qrcode_.callback = res => {
+  if (res) {
+    var currentdate = new Date(); 
+    var datetime = "Last Sync: " + currentdate.getDate() + "-"
+                + (currentdate.getMonth()+1)  + "-" 
+                + currentdate.getFullYear() + "--"  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    outputData.innerText = res;
+
+    scanning = false;
+    
+    video.srcObject.getTracks().forEach(track => {
+      track.stop();
+    });
+
+    qrResult.hidden = false;
+    canvasElement.hidden = true;
+    btnScanQR.hidden = false;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/verify", false);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        key:res,
+        date:datetime,
+
+
+}));
+    window.location.replace('/verify')
+  }
+};
+
+btnScanQR.onclick = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
+};
+
+function tick() {
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  scanning && requestAnimationFrame(tick);
+}
+
+function scan() {
+  try {
+    qrcode_.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
+  }
+}
