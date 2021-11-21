765bc02cfcc6a9a43eba5e22931c605ac93daf37
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2021-11-21 18:35:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def index():
         resp = make_response(render_template("dashboard.html",handle_catch = handle_catch ))
         verified = db.child("Users").child(uname).child("verified").get().val()
         if(verified!=1):
-            resp.set_cookie('__user__', uname)
+            resp.set_cookie('__key__', uname)
             db.child("Users").child(uname).update({
                 'verified':1
             })
@@ -46,13 +46,14 @@ def index():
             
 
 
-    if request.cookies.get('__user__')is not None:
+    if request.cookies.get('__key__')is not None:
         resp = make_response(render_template("dashboard.html",handle_catch = handle_catch))
-        verified = db.child("Users").child(request.cookies.get('__user__')).child("verified").get().val()
+        resp_err = make_response("You Are not the Verified!!!")
+        verified = db.child("Users").child(request.cookies.get('__key__')).child("verified").get().val()
         if(verified==1):
             return resp
         else:
-            return "You Are not the Verified!!!"
+            return resp_err
 
     return render_template("index.html")
 
@@ -61,7 +62,7 @@ def verify():
     if request.method == 'POST':
         value = request.get_json()
         db.child("_Attendance_").child(value['key']).set({
-            'RollNo':request.cookies.get('__user__'),
+            'RollNo':request.cookies.get('__key__'),
             "datetime":value['date'],
             "verified":1
         })
